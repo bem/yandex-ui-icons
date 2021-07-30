95ea3b3bc32a4c2db46f744f156e42bf5e3d8560
yarastqt <yarastqt@gmail.com>
2021-07-30 15:56:22 +0300

    chore: add storybook toolbar for sizes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
 module.exports = {
   stories: ['../**/*.examples.@(ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
 }
---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+export const globalTypes = {
+  size: {
+    name: 'size',
+    description: 'Icon size',
+    defaultValue: 24,
+    toolbar: {
+      icon: 'mirror',
+      items: [
+        { value: 12, right: '⚙️', title: 'Size 12' },
+        { value: 16, right: '⚙️', title: 'Size 16' },
+        { value: 24, right: '⚙️', title: 'Size 24' },
+        { value: 32, right: '⚙️', title: 'Size 32' },
+      ],
+    },
+  },
+}
