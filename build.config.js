0e95d12b6d1f7747f115fca2f79ace13bfe19b99
yarastqt <yarastqt@gmail.com>
2021-08-02 15:55:17 +0300

    build: add build tool

---
+++ build.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
+const { useCleanUpPlugin } = require('@bem-react/pack/lib/plugins/CleanUpPlugin')
+const { useCopyAssetsPlugin } = require('@bem-react/pack/lib/plugins/CopyAssetsPlugin')
+const { useTypeScriptPlugin } = require('@bem-react/pack/lib/plugins/TypeScriptPlugin')
+const { usePackageJsonPlugin } = require('@bem-react/pack/lib/plugins/PackageJsonPlugin')
+
+/**
+ * @type {import('@bem-react/pack/lib/interfaces').Config}
+ */
+module.exports = {
+  output: './dist',
+
+  plugins: [
+    useCleanUpPlugin(['./dist']),
+
+    useTypeScriptPlugin({
+      configPath: './tsconfig.prod.json',
+      onCreateSideEffects: () => false,
+    }),
+
+    useCopyAssetsPlugin([
+      {
+        context: './src',
+        src: './**/*.svg',
+        output: ['./dist', './dist/esm'],
+      },
+      {
+        src: ['./readme.md'],
+      },
+    ]),
+
+    usePackageJsonPlugin({
+      scripts: {},
+    }),
+  ],
+}
